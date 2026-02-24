741c6a176a281d474437f0eb9a52fcf6ef257e3f
michaelobum2447-sketch <michaelobum2447@gmail.com>
2026-02-24 09:10:07 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN = "8766888112:AAHYe11sKXslg5T_qRCqptoXy1txOu9e6eQ"
+TOKEN = "8766888112:AAF_9F2ZFyM2dH2MXyLml92-2wFDY-GTbkI"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
